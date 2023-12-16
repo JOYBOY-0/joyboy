ca66d47e5ffd54d7e25d447c703ab21bdf852ff0
JOYBOY-0 <falamino00@gmail.com>
2023-12-16 17:49:05 -0300

    setup

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,24 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				light: '#f9f8f3',
+				paper: '#f2eee8',
+				cream: '#fbf2e5',
+				lavender: '#8976ff',
+				candy: '#ff84eb',
+				sugar: '#ffd439',
+				'theme-bg': 'var(--theme-background)',
+				'theme-color': 'var(--theme-color)',
+				'theme-accent': 'var(--theme-accent)',
+				'theme-secondary': 'var(--theme-secondary)'
+			},
+			fontFamily: {
+				heading: 'var(--font-heading)',
+				body: 'var(--font-body)'
+			}
+		}
 	},
 	plugins: []
 }
